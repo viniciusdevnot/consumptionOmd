90b877013b17eea0440039f0b864198403788f3d
vinicius.cardoso <vinicius.cardoso@totvs.com.br>
2023-12-21 00:50:42 -0300

    up responsividade e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { StoreModule } from '@ngrx/store';
-import { FormsModule } from '@angular/forms'; // Importe o FormsModule
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './components/footer/footer.component'; // Importe o FormsModule
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
